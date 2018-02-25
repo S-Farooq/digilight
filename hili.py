e63ece25139a67355d5ea166c943ccf787c78c31
Shaham Farooq <sfarooq@daisyintel.com>
2018-02-25 02:02:06 -0500

    flask upload img attempt

--- hili.py
+++ hili.py
@@ -66,7 +66,7 @@ def get_detection_type(detect_num):
         return DETECTION_TYPES[0]
 
 def contour_img(img_path):
-    cropped_hili_img = img_path
+    cropped_hili_img = "static/testimg.jpg"
     image = cv2.imread(img_path)
 
     # rgb to HSV color spave conversion
